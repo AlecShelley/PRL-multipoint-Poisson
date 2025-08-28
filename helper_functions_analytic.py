e474a798a1529fc89bbad2c12c68d27017b3ee5b
Alec Shelley <ams01@stanford.edu>
2025-08-27 18:11:44 -0700

    split helper functions into three files

--- helper_functions_analytic.py
+++ helper_functions_analytic.py
@@ -111,6 +111,8 @@ def hyperplane_colorer_2D(points, gridpoints, colorcutoffs, salt=12345):
     )
     return regions
 
+
+
 frozen_lake_colors = ['#043765', "#edf8fe", 'green', 'purple', 'orange',\
                                               'yellow', 'pink', 'cyan', 'magenta', 'navy']
 #meat_colors = ['#d22c2c', '#f4d5ce', 'green', 'purple', 'orange', 'yellow', \
@@ -849,6 +851,139 @@ def monte_carlo_convergence_with_error_bars(d, gridpoints, samples_array, num_ru
                                                                                 alpha=0.8, edgecolor='none'))
 
     return fig, ax, all_partition_probs_mean, all_partition_probs_std
+
+def hyperplane_colorer_3D(points, gridpoints, colorcutoffs):
+    region_hashes = hyperplane_partition(points, gridpoints)
+    
+    # Use tuple-based representation instead of strings
+    region_hashes_tuples = [tuple(row) for row in region_hashes]
+    unique_dict = {}
+    inverse_indices = []
+    counter = 0
+    
+    # Manual uniqueness detection
+    for row in region_hashes_tuples:
+        if row not in unique_dict:
+            unique_dict[row] = counter
+            counter += 1
+        inverse_indices.append(unique_dict[row])
+    
+    unique_regions = list(unique_dict.keys())
+    num_unique_regions = len(unique_regions)
+
+    # Assign random colors
+    colors = np.random.uniform(size=num_unique_regions)
+    color_indices = np.digitize(colors, colorcutoffs)
+
+    # Map color indices to gridpoints
+    regions = np.array([color_indices[inverse_indices[i]] for i in range(len(gridpoints))], dtype=np.int32)
+    return regions
+
+def colors_mc(d, gridpoints, color_dist, colors, samples_array):
+    """Returns the CPF of gridpoints[-1] having the color corresponding to colorcutoffs[-1],
+         given the colors of the other points
+        d: dimension of the hyperplane
+        r: radius to generate the hyperplanes
+        gridpoints: numpy array of points to make the distribution of colors for
+        color_dist: tuple, probability distribution of colors
+        colors: tuple of colors for points[:-1]
+        samples_array: array of number of samples to run the simulation, make with np.logspace!
+
+        returns: list of dictionaries of color probabilities for each color in colors, for each sample size"""
+    
+    colorcutoffs = np.cumsum(color_dist)[:-1] #cumulative sum of color distribution to get cutoff values for each color
+    r = max(np.linalg.norm(p) for p in gridpoints)*1.25 #maximum distance from origin
+    if d == 2:
+        hyperplane_colorer = hyperplane_colorer_2D
+    elif d == 3:
+        hyperplane_colorer = hyperplane_colorer_3D
+
+            
+    conditional_count = 0 #this counts the number of times colors[::-1] is the colors of gridpoints[::-1]
+
+    color_counts = np.zeros(len(color_dist)) #array to store the number of times each color is the color of gridpoints[-1]
+    color_probs = np.zeros((len(color_dist), len(samples_array))) #array to store the probability of each color for each sample size
+
+    for i, num_samples in enumerate(np.diff(np.insert(samples_array, 0, 0))):
+        for _ in range(num_samples):
+            n = np.random.poisson(rate(d,r)) #number of hyperplanes
+            points = sample_from_ball(d, n)*r #sample points defining hyperplanes
+
+            color_values = hyperplane_colorer(points, gridpoints, colorcutoffs)
+            if np.all(color_values[:-1] == colors):
+                conditional_count += 1
+                color_counts[color_values[-1]] += 1 #one new recorded instance of the color of gridpoints[-1]
+        color_probs[:,i] = color_counts/conditional_count if conditional_count > 0 else 0
+
+    return color_probs
+
+def plot_mc_colors_with_errorbars(d, gridpoints, color_dist, colors,\
+                                  samples_array, num_runs, analytic_probs=False):
+    """Wrapper function that runs the convergence multiple times and plots the average with error bars.
+    d: dimension of the hyperplane
+    gridpoints: numpy array of points, all but the last are conditioned on with colors from "colors"
+    color_dist: tuple, the probability distribution of colors
+    colors: tuple of colors for points[:-1]
+    samples_array: array of number of samples to run the simulation
+    num_runs: number of epochs, used to calculate the mean and standard deviation"""
+
+    # Run the Monte Carlo convergence multiple times
+    color_probs_sum = np.zeros((len(color_dist), len(samples_array)))
+    color_probs_sq_sum = np.zeros((len(color_dist), len(samples_array)))
+    for _ in range(num_runs):
+        color_probs = colors_mc(d, gridpoints, color_dist, colors, samples_array)
+        color_probs_sum += color_probs
+        color_probs_sq_sum += color_probs**2
+
+    # Calculate the mean and standard deviation for each color
+    color_probs_mean = color_probs_sum / num_runs
+    variance = (color_probs_sq_sum / num_runs) - (color_probs_mean ** 2)
+    stdev = np.sqrt(variance)
+
+    # Calculate analytic probabilities if requested
+    analytic_color_dist = None
+    if analytic_probs:
+        analytic_color_dist = color_distribution(gridpoints, colors, color_dist)
+
+    # Plot the mean with error bars
+    fig, ax = plt.subplots(figsize=(12, 8))
+    for color in range(len(color_dist)):
+        ax.errorbar(samples_array, color_probs_mean[color], yerr=stdev[color],
+                     marker='o', label=f"Color {color}", markersize=2, capsize=3)
+
+    if analytic_probs:
+        label_added = False
+        for color in range(len(color_dist)):
+            if not label_added:
+                ax.axhline(y=analytic_color_dist[color], linestyle='--', color='grey', label='Analytic Solution')
+                label_added = True  # Add the label only once
+            else:
+                ax.axhline(y=analytic_color_dist[color], linestyle='--', color='grey')
+
+    ax.set_xlabel('Number of Samples', fontsize=20)
+    ax.set_ylabel('Probability of Color', fontsize=20)
+    ax.set_title(f'Color of {list(map(list,gridpoints))[-1]} Given {list(map(list,gridpoints))[:-1]}', fontsize=26)
+    ax.legend(loc='upper left', borderaxespad=0., fontsize=20)
+    ax.tick_params(axis='both', which='major', labelsize=14)  # Major ticks
+    ax.tick_params(axis='both', which='minor', labelsize=12)  # Minor ticks
+    ax.set_xscale('log')
+
+    # Add inset for log error
+    if analytic_probs:
+        inset_ax = fig.add_axes([0.5, 0.4, 0.25, 0.25], facecolor="white", alpha = .3)  # [left, bottom, width, height]
+        inset_ax.patch.set_alpha(.4)  # Adjust transparency of the inset background
+        for color in range(len(color_dist)):
+            log_error = np.log10(np.abs(analytic_color_dist[color] - color_probs_mean[color]))
+            inset_ax.plot(samples_array, log_error, label=f"Color {color}")
+        
+        inset_ax.set_xscale('log')
+        inset_ax.set_ylabel(r'$\log_{10}$ Error', fontsize=20)
+        inset_ax.tick_params(axis='both', which='major', labelsize=14)
+        inset_ax.text(0.5, 0.05, 'Samples', fontsize=20, transform=inset_ax.transAxes,
+                        ha='center', va='bottom', color='black', bbox=dict(facecolor='white', \
+                                                                           alpha=0.8, edgecolor='none'))
+
+    return fig, ax, color_probs_mean, stdev
 
 if __name__ == "__main__":
     import doctest
